701e3565ab6107785281ec8ce8a060fe5a71e9e7
Adi Gonnen <adigonnen@gmail.com>
2018-06-17 00:06:39 +0300

    fix the responsive

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 'use strict'
 
-function toggleNavBar() {
+
+//open/close hamburger menu
+function toggleNavBar() {           
     document.querySelector('.fa').classList.toggle('fa-times');         //hamburger
     document.querySelector('.fa').classList.toggle('fa-bars');          //X
     document.querySelector('.nav').classList.toggle('navbar-open');     //culomn navbar
@@ -8,17 +10,19 @@ function toggleNavBar() {
     document.querySelector('.main-blogs').classList.toggle('opacity');
 }
 
+//open read more modal
 function openModal() {
     document.querySelector('.modal-read-more').classList.remove('hide');
     document.querySelector('.main-blogs').classList.add('opacity');
 }
 
+//close read more modal
 function closeModal() {
     document.querySelector('.modal-read-more').classList.add('hide');
     document.querySelector('.main-blogs').classList.remove('opacity');
 }
 
+//open/close jurnal menu
 function toggleDropdown() {
     document.querySelector('.modal-dropdown').classList.toggle('hide');
-    // document.querySelector('.navbar-open').classList.toggle('opacity');
 }
